88cd2abdf32548536a35df141c49a1288e0dc206
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-11 10:57:15 -0300

    Primeiro Commit

---
+++ ScopeEstudo/src/main/java/Springboot/ScopeEstudo/ScopeEstudoApplication.java
@@ -0,0 +1,13 @@
+package Springboot.ScopeEstudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopeEstudoApplication.class, args);
+	}
+
+}
---
+++ ScopeEstudo/src/test/java/Springboot/ScopeEstudo/ScopeEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot.ScopeEstudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopeEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
